95bb70fbb6318a1ac9186f48d64b707211e2c368
Lily <lili.korgik@gmail.com>
2023-08-10 14:07:29 +0300

    New changing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Person nelka = new Person ();
+      /*  Person nelka = new Person ();
         nelka.name = "Nelka";
         nelka.age = 28;
 
@@ -18,7 +18,96 @@ public class Main {
         System.out.println(nelka.age);
 
         System.out.println(max.name);
-        System.out.println(max.age);
+        System.out.println(max.age); */
+
+
+
+      /*  System.out.println("Tell me why");
+        System.out.println("Ain't nothin' but a heartache");
+
+        System.out.print("I believe i can fly");
+        System.out.println(" i believe i can touch the sky");
+
+        byte oneByte = 1;
+        short twoByte = 2;
+        int fourByte = 4;
+        long eightByte = 8;
+
+        float fourByteWithDot = 4.89f;
+        double eightByteWithDot = 8.38;
+
+        char oneByteCharter = 36;
+
+        boolean OneByteFalseOrTreu = true;
+
+        System.out.println (oneByte);
+        System.out.println (twoByte);
+        System.out.println (fourByte);
+        System.out.println (eightByte);
+
+        System.out.println (fourByteWithDot);
+        System.out.println (eightByteWithDot);
+
+        System.out.println (oneByteCharter);
+
+        System.out.println (OneByteFalseOrTreu);  */
+
+
+
+
+        int a = 2;
+        int b = 8;
+        int c = 64;
+        int y = 9;
+
+        int result1;
+        int result2;
+        int result3;
+        int result4;
+        int result5;
+
+        result1 = a + b;
+        result2 = c - b;
+        result3 = a * b;
+        result4 = c / b;
+        result5 = c % y;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+
+        System.out.println(y % 2 == 0);
+        System.out.println(y % 2 == 1);
+
+
+
+        int x = a + b;
+        int k = (x - 9) * 5;
+
+        System.out.println(k);
+
+
+
+        System.out.println(c > b);
+        System.out.println(a < y);
+        System.out.println(a == b);
+        System.out.println(y >= b);
+        System.out.println(c <= y);
+        System.out.println(a != c);
+
+
+
+        int boxVolume;
+        int n = 5;
+        int m = 6;
+        int l = 7;
+
+        boxVolume = n*m*l;
+
+        System.out.println (boxVolume);
+
 
 
 
@@ -26,3 +115,4 @@ public class Main {
 
     }
 }
+
