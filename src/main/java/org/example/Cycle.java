b934fc1b263344f3b90d130d28123eb0a23fea5b
Lily <lili.korgik@gmail.com>
2023-08-15 11:38:59 +0300

    Added cycles

---
+++ src/main/java/org/example/Cycle.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Cycle {
+
+    public static void main(String[] args) {
+
+      //  for (int i = 0; i <= 10; i++) {
+      //  System.out.println(i);
+      //  }
+
+      //  for (int e = 6; e > -5; e--) {
+        // System.out.println(e);
+      //  }
+
+
+
+     //   int age = 18;
+     //   while (age < 65){
+     //   age = age + 23;
+       // System.out.println(age);
+      //  }
+
+
+        int number = 6;
+
+        do {
+            System.out.print(number);
+
+        } while (number != 0);
+
+    }
+
+
+    }
+
+
